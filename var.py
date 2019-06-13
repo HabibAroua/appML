7ee1b72258a53e2ee48cb5890e2ded5534177478
HabibAroua <habibha.aroua82@gmail.com>
2019-06-13 01:13:07 +0100

    learn how enter three values on one line

---
+++ var.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+    #varibale are memory location store values
+    
+print 'variable'
+
+#you should enter your name , age and your marks in one line
+
+name , age , marks = 'Habib' , 25 , 19.5
+
