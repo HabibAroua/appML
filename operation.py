773909a18be7f11466428da3161b372424738fce
HabibAroua <habibha.aroua82@gmail.com>
2019-06-13 02:06:07 +0100

    operation in python

---
+++ operation.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+x=15
+y=5
+print x+y
+print x-y
+print x*y
+print x/y
+print x%y
+
+sentence="Hello everyone my name is Habib Aroua, I'm From Tunisia"
+
+print sentence
+print sentence[1]
+print sentence[2:5]
+print sentence[:4]
+print sentence[:-2]
+
