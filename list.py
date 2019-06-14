af3cb8b7e45201105ae0ba55618d5d4cd796258c
HabibAroua <habibha.aroua82@gmail.com>
2019-06-15 00:15:44 +0100

    list

---
+++ list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+listName= ["Habib" , "Safa" , "Nada" , "Salah"]
+print listName
+print listName[1]
+listName.append("Laila")
+print listName
+
+del listName[2]
+
+print listName
+
