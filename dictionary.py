5384648e4cd33eedc3d55f89fb1c6d79b174730f
HabibAroua <habibha.aroua82@gmail.com>
2019-06-18 23:48:51 +0100

    dictionary

---
+++ dictionary.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+students=["Habib",24,"Suzanne",23,"Eya",22]
+print students
+students={"Habib":24 , "Safa":27 , "Nada":23}
+print students
+print students["Nada"]
+print len(students)
+del students["Nada"] #[key]
+print students
