31eee47c55176f6c5e4205cec1573ada2512484f
HabibAroua <habibha.aroua82@gmail.com>
2019-06-21 15:42:21 +0100

    while finish

--- while.py
+++ while.py
@@ -1,13 +1,27 @@
 #!/usr/bin/env python
-print "Example 1"
+print "Example normal"
 c=0
 while c<5:
     print c
     c=c+1
-print "Example 2"
+print "Example using break"
 c=0
 while c<5:
     if c==3:
         break
     print c
     c=c+1
+print "Example using continue"
+c=0
+while c<5:
+    c=c+1
+    if c==3:
+        continue
+    print c
+print "Example using pass"
+c=0
+while c<5:
+    c=c+1
+    if c==3:
+        pass
+    print c
