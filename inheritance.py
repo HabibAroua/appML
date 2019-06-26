dbe78ac139201a2354e1f9e0b4699f467d4ea3bd
HabibAroua <habibha.aroua82@gmail.com>
2019-06-26 02:59:55 +0100

    inheritance

--- inheritance.py
+++ inheritance.py
@@ -7,3 +7,10 @@ class Parent:
         print "This is the parent function"
 
 p=Parent()
+
+class Child(Parent):
+    def __init__(self):
+        print "This is the child class"
+
+    def childFunction(self):
+        print "This is the dhild function"
