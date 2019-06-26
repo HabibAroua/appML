3c9110c9e586351761997610ebae8c5dceceffdd
HabibAroua <habibha.aroua82@gmail.com>
2019-06-26 01:02:58 +0100

    inheritance

---
+++ inheritance.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+class Parent:
+    def __init__(self):
+        print "This is the parent class"
+
+    def parentFunction(self):
+        print "This is the parent function"
+
+p=Parent()
