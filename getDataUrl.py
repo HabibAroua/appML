58018d40163f28ba2efef7eb030a6b12a9ffbf00
HabibAroua <habibha.aroua82@gmail.com>
2019-11-17 16:05:45 +0100

    finish this program

--- getDataUrl.py
+++ getDataUrl.py
@@ -19,10 +19,10 @@ def writeFile(fileName,line):
     except Exception as e : 
         print("This file is not found : "+str(e))
         
-def addNewLineInFile(fileName,name):
+def addNewLineInFile(fileName,line):
     try:
         hs = open(fileName,"a")
-        hs.write(name+"\n")
+        hs.write(line+"\n")
         hs.close()
     except Exception as e : 
         print(str(e))
@@ -35,11 +35,19 @@ def createNewFile(fileName):
     
 def main():
     try:
+        choice = input("Do you want create new file Y/N : ")
+        if choice == 'Y' or choice == 'y':
+            createNewFile(input("File name : "))
         c='Y'
         while c=='y' or c=='Y' :
             url = input("Enter an URL : ")
-            x = urllib.request.urlopen(url) 
-            print(x.read())
+            x = urllib.request.urlopen(url)
+            s=x.read()
+            print(s)
+            choice='N'
+            choice = input ("Do you want to store this content in file ? : Y/N ")
+            if choice == 'Y' or choice == 'y' :
+                addNewLineInFile(input('File name : '),str(s))
             c = input("Continue ? Y/N : ")
   
     except Exception as e : 
