c81c45b8acd79b39fa1ddecc92992d6cf3ff1c89
HabibAroua <habibha.aroua82@gmail.com>
2019-11-17 12:59:04 +0100

    finish dev all method of file

--- io.py
+++ io.py
@@ -1,4 +1,17 @@
+def readFile(fileName):
+    with open(fileName,'r') as f:
+        for line in f:
+            print(line, end='')
 
-with open('test.txt','r') as f:
-    for line in f:
-        print(line,end='')
+def writeFile(fileName,line):        
+    with open(fileName,'w') as f:
+        f.write(line)
+        
+def addNewLineInFile(fileName,name):
+    hs = open(fileName,"a")
+    hs.write(name+"\n")
+    hs.close()
+    
+    
+addNewLineInFile("test.txt","Habib")
+readFile("test.txt")
