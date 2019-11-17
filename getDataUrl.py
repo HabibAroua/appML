d02f4f044971db3c09cb041edb36cf801d11734c
HabibAroua <habibha.aroua82@gmail.com>
2019-11-17 11:56:06 +0100

    add new file which can read url link

---
+++ getDataUrl.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+import urllib.request 
+import urllib.parse 
+  
+try: 
+    x = urllib.request.urlopen('https://www.google.com') 
+    print(x.read()) 
+  
+except Exception as e : 
+    print(str(e)) 
