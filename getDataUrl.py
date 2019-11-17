03da5cf0170291eb5217282a74b83df9cbfdc3c7
HabibAroua <habibha.aroua82@gmail.com>
2019-11-17 13:10:44 +0100

    add exceptions

--- getDataUrl.py
+++ getDataUrl.py
@@ -4,6 +4,23 @@
 import urllib.request 
 import urllib.parse 
 
+def readFile(fileName):
+    with open(fileName,'r') as f:
+        for line in f:
+            print(line, end='')
+
+def writeFile(fileName,line):        
+    with open(fileName,'w') as f:
+        f.write(line)
+        
+def addNewLineInFile(fileName,name):
+    hs = open(fileName,"a")
+    hs.write(name+"\n")
+    hs.close()
+    
+def createNewFile(fileName):
+    f= open(fileName,"w+")
+    
 def main():
     try:
         c='Y'
