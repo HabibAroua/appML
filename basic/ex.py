ee6a94e262ecfefdffdc7748e1ca77f316f4a731
HabibAroua <habibha.aroua82@gmail.com>
2020-02-06 20:37:13 +0100

    anter argument

---
+++ basic/ex.py
@@ -0,0 +1,4 @@
+list1=["Nada","Sana","Mariem","Siwar"]
+
+for i in range(0,3):
+    print list1[i]
---
+++ ex.py
@@ -0,0 +1,4 @@
+list1=["Nada","Sana","Mariem","Siwar"]
+
+for i in range(0,len(list1)):
+    print list1[i]
