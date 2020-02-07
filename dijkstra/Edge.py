877fa3b463448b562189daa1a7b5c6b96255a6c1
HabibAroua <habibha.aroua82@gmail.com>
2020-02-07 22:55:42 +0100

    dev Edge class

---
+++ dijkstra/Edge.py
@@ -0,0 +1,6 @@
+class Edge(Object):
+    
+    def __init__(self , weight , startVertex , targetVertex):
+        self.weight = weight;
+        self.startVertex = startVertex;
+        self.targetVertex = targetVertex;
