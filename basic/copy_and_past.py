01632e8813c5dffefe946e9933e4124357d75975
HabibAroua <habibha.aroua82@gmail.com>
2020-06-03 14:12:17 +0100

    file path

---
+++ basic/copy_and_past.py
@@ -0,0 +1,6 @@
+import shutil
+
+original = r'/home/habib/Downloads/andy_n.jpg'
+target = r'/home/habib/Documents/tuto_python/basic/img/andy_n.jpg'
+
+shutil.copyfile(original, target)
---
+++ basic/get_path.py
@@ -0,0 +1,5 @@
+import os
+
+print('getcwd:      ', os.getcwd())
+x=__file__
+print('__file__:    ', __file__)
